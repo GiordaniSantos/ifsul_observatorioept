65b9b70cde8800805a4c74ed44303d4dbf4a5d7d
GiordaniSantos <dani.santossilveira@hotmail.com>
2021-05-07 17:27:33 -0300

    Sign up implemented

--- frontend/src/screens/SignUp/index.js
+++ frontend/src/screens/SignUp/index.js
@@ -1,31 +1,41 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { signUp } from './SignUpActions';
 
 const SignUp = () => {
+
+    const submitHandler = (e) => {
+        e.preventDefault();
+
+        const formData = new FormData(e.target);
+        const data = Object.fromEntries(formData);
+
+        console.log(data);
+        
+        signUp(data);
+    };
+
     return(
         <div className="">
             <h1>Cadastrar</h1>
             <div>
-                <form>
+                <form onSubmit={submitHandler}>
                     <div className="">
                         <label>Nome</label>
-                        <input type="text" />
+                        <input type="text" name="name" />
                     </div>
                     <div className="">
                         <label>Email</label>
-                        <input type="text" />
+                        <input type="text" name="email" />
                     </div>
                     <div className="">
                         <label>Senha</label>
-                        <input type="password" />
+                        <input type="password" name="password" />
                     </div>
                     <div className="">
-                        <label>Confirmar de Senha</label>
-                        <input type="password" />
-                    </div>
-                    <div className="">
-                        <label>Instituição Associada</label>
-                        <input type="text" />
+                        <label>Confirmar Senha</label>
+                        <input type="password" name="password_confirmation" />
                     </div>
                     <div>
                         <button>Cadastrar</button>
@@ -39,4 +49,9 @@ const SignUp = () => {
     );
 };
 
-export default SignUp;
+const mapStateToProps = (state) => {
+    return { account: state.signUp.account }
+};
+
+
+export default connect(mapStateToProps, {signUp})(SignUp);
